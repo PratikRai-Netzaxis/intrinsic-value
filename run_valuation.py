2d2d1d1f6291fe6d032ab6b62ff643b279d2feca
Pratik Rai <info@netzaxis.com>
2021-01-17 20:01:03 +0000

    Implement valuation for single company in jupyter notebook

--- run_valuation.py
+++
@@ -1,8 +0,0 @@
-from Stock import Stock
-from us_yield import get_10y_us_bond_yield
-
-ticker = 'AAPL'
-company_name = 'Apple'
-risk_free_return = get_10y_us_bond_yield()
-
-stock_details = Stock(ticker, company_name, risk_free_return)
