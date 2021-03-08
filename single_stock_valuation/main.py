6f74f82e7da3d605e9961442088debc2315a247a
Pratik Rai <info@netzaxis.com>
2021-03-08 21:35:13 +0000

    handle http request in single stock valuation test

--- single_stock_valuation/main.py
+++ single_stock_valuation/main.py
@@ -2,7 +2,7 @@ from Stock import Stock
 from utilities.downloaders.us_yield import get_10y_us_bond_yield
 
 
-def print_stock_valuation(stock):
+def get_stock_valuation_str_formated(stock):
     message = f"""
     {stock.ticker}
     Stock price: {stock.price}
@@ -10,14 +10,14 @@ def print_stock_valuation(stock):
     Potential upside: {stock.potential_upside}
     Average recommendation: {stock.recommendation_rating}
     """
-    print(message)
+    return message
 
 
-def run_single_company_valuation():
+def run_single_company_valuation(request):
     ticker = 'AAPL'
     company_name = 'Apple inc.'
     risk_free_return = get_10y_us_bond_yield()
 
     stock = Stock(ticker, company_name, risk_free_return)
 
-    print_stock_valuation(stock)
+    return get_stock_valuation_str_formated(stock)
